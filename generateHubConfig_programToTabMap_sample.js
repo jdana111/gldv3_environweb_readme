12190ee585bb793d28559c219c86e1d44c252742
John Dana <john@danasd.com>
2025-04-14 03:21:45 -0600

    Houseclean generateHubConfig_programToTabMap_sample.js

--- generateHubConfig_programToTabMap_sample.js
+++ generateHubConfig_programToTabMap_sample.js
@@ -1,37 +1,37 @@
-const programToTabMap = {
+programToTabMap = {
   P1: {
-    Inspection: "DEFAULT",
-    Asset: "CUSTOM_CN",
-    Contact: "DEFAULT",
-    PermitHistory: "DEFAULT",
+    Inspection: 'DEFAULT',
+    Asset: 'CUSTOM_CN',
+    Contact: 'DEFAULT',
+    PermitHistory: 'DEFAULT'
   },
   P2: {
-    Asset: "CUSTOM_PT",
-    Contact: "DEFAULT",
-    Permit: "DEFAULT",
-    Substance: "DEFAULT",
-    Communication: "DEFAULT",
-    Photo: "DEFAULT",
+    Asset: 'CUSTOM_PT',
+    Contact: 'DEFAULT',
+    Permit: 'DEFAULT',
+    Substance: 'DEFAULT',
+    Communication: 'DEFAULT',
+    Photo: 'DEFAULT'
   },
   P3: {
-    Inspection: "DEFAULT",
-    Asset: "CUSTOM_PP",
-    AssetNonStructuralBMP: "DEFAULT",
-    Contact: "DEFAULT",
-    Communication: "DEFAULT",
-    Photo: "DEFAULT",
+    Inspection: 'DEFAULT',
+    Asset: 'CUSTOM_PP',
+    AssetNonStructuralBMP: 'DEFAULT',
+    Contact: 'DEFAULT',
+    Communication: 'DEFAULT',
+    Photo: 'DEFAULT'
   },
   P4: {
-    Inspection: "DEFAULT",
-    Contact: "DEFAULT",
-    Substance: "DEFAULT",
-    Communication: "DEFAULT",
-    Photo: "DEFAULT",
+    Inspection: 'DEFAULT',
+    Contact: 'DEFAULT',
+    Substance: 'DEFAULT',
+    Communication: 'DEFAULT',
+    Photo: 'DEFAULT'
   },
   P5: {
-    Asset: "CUSTOM_PC",
-    Contact: "DEFAULT",
-    Communication: "DEFAULT",
-    Photo: "DEFAULT",
-  },
-};
+    Asset: 'CUSTOM_PC',
+    Contact: 'DEFAULT',
+    Communication: 'DEFAULT',
+    Photo: 'DEFAULT'
+  }
+}
