6c0ec9a25e0db766a900696d98ed7d4cd5f27e81
John Dana <john@danasd.com>
2025-04-15 12:33:34 -0600

    Add generateHubConfig_tabsDetail_sample.js with tab details and configurations

---
+++ generateHubConfig_tabsDetail_sample.js
@@ -0,0 +1,254 @@
+const tabsDetail = {
+  "Inspection_DEFAULT": {
+    "id": 0,
+    "columns": [
+      {
+        "field": "inspectionDateTime",
+        "headerName": "Date / Time",
+        "widths": {
+          "sm": 100,
+          "md": 150,
+          "lg": 180
+        }
+      },
+      {
+        "field": "inspectorName",
+        "headerName": "Inspector",
+        "widths": {
+          "sm": 100,
+          "md": 150,
+          "lg": 180
+        }
+      },
+      {
+        "field": "inspectorTypeName",
+        "headerName": "Inspection Type",
+        "widths": {
+          "sm": 100,
+          "md": 150,
+          "lg": 180
+        }
+      },
+      {
+        "field": "notes",
+        "headerName": "Notes",
+        "widths": {
+          "sm": 0,
+          "md": 0,
+          "lg": 180
+        }
+      }
+    ],
+    "tabLabel": "Inspections",
+    "tabAbbreviation": "INSP",
+    "datasetSingular": "inspection",
+    "datasetPlural": "inspections"
+  },
+  "Asset_DEFAULT": {
+    "id": 1,
+    "columns": [
+      {
+        "field": "assetTypeName",
+        "headerName": "Asset Type",
+        "widths": {
+          "sm": 101,
+          "md": 151,
+          "lg": 181
+        }
+      },
+      {
+        "field": "assetStatusName",
+        "headerName": "Asset Status",
+        "widths": {
+          "sm": 101,
+          "md": 151,
+          "lg": 181
+        }
+      },
+      {
+        "field": "location",
+        "headerName": "Location",
+        "widths": {
+          "sm": 101,
+          "md": 151,
+          "lg": 181
+        }
+      },
+      {
+        "field": "notes",
+        "headerName": "Notes",
+        "widths": {
+          "sm": 0,
+          "md": 0,
+          "lg": 181
+        }
+      }
+    ],
+    "tabLabel": "Assets",
+    "tabAbbreviation": "ASST",
+    "datasetSingular": "asset",
+    "datasetPlural": "assets"
+  },
+  "AssetNonStructuralBMP_DEFAULT": {
+    "id": 2,
+    "columns": [],
+    "tabLabel": "",
+    "tabAbbreviation": "",
+    "datasetSingular": "",
+    "datasetPlural": ""
+  },
+  "Contact_DEFAULT": {
+    "id": 3,
+    "columns": [
+      {
+        "field": "lastName",
+        "headerName": "Last Name",
+        "widths": {
+          "sm": 102,
+          "md": 152,
+          "lg": 182
+        }
+      },
+      {
+        "field": "firstName",
+        "headerName": "First Name",
+        "widths": {
+          "sm": 102,
+          "md": 152,
+          "lg": 182
+        }
+      },
+      {
+        "field": "contactTypeName",
+        "headerName": "Contact Type",
+        "widths": {
+          "sm": 102,
+          "md": 152,
+          "lg": 182
+        }
+      },
+      {
+        "field": "emailAddress",
+        "headerName": "Email Address",
+        "widths": {
+          "sm": 102,
+          "md": 152,
+          "lg": 182
+        }
+      },
+      {
+        "field": "notes",
+        "headerName": "Notes",
+        "widths": {
+          "sm": 0,
+          "md": 0,
+          "lg": 182
+        }
+      }
+    ],
+    "tabLabel": "Contacts",
+    "tabAbbreviation": "CONT",
+    "datasetSingular": "contact",
+    "datasetPlural": "contacts"
+  },
+  "Permit_DEFAULT": {
+    "id": 4,
+    "columns": [],
+    "tabLabel": "",
+    "tabAbbreviation": "",
+    "datasetSingular": "",
+    "datasetPlural": ""
+  },
+  "PermitHistory_DEFAULT": {
+    "id": 5,
+    "columns": [
+      {
+        "field": "permitStatusDate",
+        "headerName": "Permit Status Date",
+        "widths": {
+          "sm": 103,
+          "md": 153,
+          "lg": 183
+        }
+      },
+      {
+        "field": "permitStatus",
+        "headerName": "Permit Status",
+        "widths": {
+          "sm": 103,
+          "md": 153,
+          "lg": 183
+        }
+      },
+      {
+        "field": "notes",
+        "headerName": "Notes",
+        "widths": {
+          "sm": 0,
+          "md": 0,
+          "lg": 183
+        }
+      }
+    ],
+    "tabLabel": "Permits",
+    "tabAbbreviation": "PRMT",
+    "datasetSingular": "permitHistory",
+    "datasetPlural": "permitHistory"
+  },
+  "Substance_DEFAULT": {
+    "id": 6,
+    "columns": [],
+    "tabLabel": "",
+    "tabAbbreviation": "",
+    "datasetSingular": "",
+    "datasetPlural": ""
+  },
+  "Transaction_DEFAULT": {
+    "id": 7,
+    "columns": [],
+    "tabLabel": "",
+    "tabAbbreviation": "",
+    "datasetSingular": "",
+    "datasetPlural": ""
+  },
+  "Communication_DEFAULT": {
+    "id": 8,
+    "columns": [],
+    "tabLabel": "",
+    "tabAbbreviation": "",
+    "datasetSingular": "",
+    "datasetPlural": ""
+  },
+  "Photo_DEFAULT": {
+    "id": 9,
+    "columns": [],
+    "tabLabel": "",
+    "tabAbbreviation": "",
+    "datasetSingular": "",
+    "datasetPlural": ""
+  },
+  "Asset_CUSTOM_CN": {
+    "id": 10,
+    "columns": [],
+    "tabLabel": "BMPs",
+    "tabAbbreviation": "BMP",
+    "datasetSingular": "",
+    "datasetPlural": ""
+  },
+  "Asset_CUSTOM_PT": {
+    "id": 11,
+    "columns": [],
+    "tabLabel": "Devices",
+    "tabAbbreviation": "DVC",
+    "datasetSingular": "",
+    "datasetPlural": ""
+  },
+  "Asset_CUSTOM_PP": {
+    "id": 12,
+    "columns": [],
+    "tabLabel": "BMPs",
+    "tabAbbreviation": "BMP",
+    "datasetSingular": "",
+    "datasetPlural": ""
+  }
+}
